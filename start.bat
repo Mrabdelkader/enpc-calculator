367644b1818250eb723d78e6d675de268447a74c
Mrabdelkader <rabhallah.abdelkader.25@gmail.com>
2025-02-24 23:44:03 +0100

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,10 +1,11 @@
 @echo off
-REM Create and activate virtual environment
-python -m venv venv
-call venv\Scripts\activate
-
+REM Check if pip is installed
+pip --version
+IF %ERRORLEVEL% NEQ 0 (
+    echo Pip is not installed. Installing pip...
+    python -m ensurepip
+)
 REM Install required packages
 pip install -r requirements.txt
-
 REM Run the application
 python sys.py
